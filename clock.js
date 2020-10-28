50a5dc93db2e04b2e0c11001c50d192f72b5247f
nayoung511 <nayoungbae511@gmail.com>
2020-10-29 02:38:15 +0900

    todo list, geolocation, background change, time shown

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js-clock");
+    clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minutes= date.getMinutes();
+    const hour = date.getHours();
+    const seconds = date.getSeconds();
+    
+    clockTitle.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
